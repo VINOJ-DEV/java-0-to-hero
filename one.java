26619ab87280fbe8bd7c10bafdb08930445104d9
VINOJ-DEV <230801238@rajalakshmi.edu.in>
2025-07-06 22:24:32 +0530

    The start

---
+++ one.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main (String args[]){
+        int container = 49;
+        System.out.print(container);
+        String container2 = "john";
+        System.out.print(container2);
+    }
+}
