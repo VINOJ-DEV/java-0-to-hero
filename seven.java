7d02d718942323867c910532a30a0822c7d4844d
VINOJ-DEV <230801238@rajalakshmi.edu.in>
2025-07-09 19:18:58 +0530

    primary  datatypes

---
+++ seven.java
@@ -0,0 +1,20 @@
+/// NOTE  1.  Single quotation for char ' '
+///       2.  f or F at last for float number 
+///       3.  String  has            variable.charAT(index)
+import java.lang.System;
+class hello{
+      public static void main(String args[])
+      {
+        float a = 1.123456f;
+        char b = 'v';
+        String var = "vinoj";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(var);
+        System.out.println(var.charAt(0));
+
+        Boolean c = true;   /// or false also
+        System.out.println(c);
+      }
+}
