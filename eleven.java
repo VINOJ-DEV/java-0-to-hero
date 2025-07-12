1621f31cb50ea28d46ff1900341ab99f337f88ae
VINOJ-DEV <230801238@rajalakshmi.edu.in>
2025-07-12 21:08:27 +0530

    gaining skill

---
+++ eleven.java
@@ -0,0 +1,14 @@
+// ternary operator
+import java.lang.System;
+import java.util.Scanner;
+
+class ternary{
+    public static void main(String args[])
+    {
+        Scanner vinoj = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = vinoj.nextInt();
+
+        System.out.println(num >10 ? "Greater than 10" : "Lesser than 10");
+    }
+}
