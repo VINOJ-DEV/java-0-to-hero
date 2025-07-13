173e03a489b4c35c285ad3b581538100aa517ce8
VINOJ-DEV <230801238@rajalakshmi.edu.in>
2025-07-13 21:53:10 +0530

    for loop

---
+++ twelve.java
@@ -0,0 +1,16 @@
+
+
+import java.lang.System;
+import java.util.Scanner;
+
+class forloop{
+    public static void main(String args[])
+    {
+        Scanner vinoj = new Scanner (System.in);
+        int a = vinoj.nextInt();
+
+        for (int i =0;i<=5;i++){
+            System.out.println(a);
+        }
+    }
+}
