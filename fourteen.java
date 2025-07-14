24126f223c6c6aab18fe3ac9a733d8951d8244de
VINOJ-DEV <230801238@rajalakshmi.edu.in>
2025-07-14 20:42:26 +0530

    solution

---
+++ fourteen.java
@@ -0,0 +1,13 @@
+// print even numbers between 1 to 10
+
+class sol {
+    public static void main(String args[])
+    {
+        for (int i=1;i<=10;i++)
+        {
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
