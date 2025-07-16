fb298533f2344eb3fa6e407bfad440ac9faf9a8c
VINOJ-DEV <230801238@rajalakshmi.edu.in>
2025-07-16 21:50:18 +0530

    array

---
+++ sixteen.java
@@ -0,0 +1,12 @@
+// A data structure that allows to store a collection of elements of the same type , under a single variable name
+ class demo {
+    public static void main(String args[])
+    {
+        String[] song = new String[3];
+        song[0]="s1";
+        song[1]="s2";
+        song[2]="s3";
+
+        System.out.println(song[2]);
+    }
+ }
