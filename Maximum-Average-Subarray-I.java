049c4fd16032f4c1417feb2714abdd2390dc8400
VINOJ S A <230801238@rajalakshmi.edu.in>
2025-07-23 19:54:40 +0530

    [643] [Time Beats: 77.18%] [Memory Beats: 82.42%] - LeetPush

---
+++ Maximum-Average-Subarray-I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        // need 1.array   2.window size  3.variable for our need (i.e sum,add,subract....etc)
+        double avg =0;
+
+        //step1 : 1st window
+        for(int i=0;i<k;i++){ avg+=nums[i];} 
+        double max = avg;   
+        
+        //step2 : move the window and check ,   i.e remove left-side and add right-side elements
+        //step3 : store needed in seperate variable i.e here max
+        for(int i=k;i<nums.length;i++){
+            avg = avg - nums[i-k] +nums[i];
+            if(max<avg)
+             {max=avg;}
+        }
+
+
+        return max/k;
+    }
+}
