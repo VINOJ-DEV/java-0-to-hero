219a5df2bc40e1f81e55a5e117c626d9105e6819
VINOJ S A <230801238@rajalakshmi.edu.in>
2025-07-24 21:27:40 +0530

    [1456] [Time Beats: 53.60%] [Memory Beats: 84.34%] - LeetPush

--- Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.java
+++ Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.java
@@ -1,16 +1,20 @@
+// 0 basic variables
+// 1st window in max  and increase count , then only u can remove left side elements representation
+// 2nd  next windows , move one by one till last , so i<length of s
+// for integer type -->  count = count - isvow(s.charAt(i-k)) + isvow(s.charAt(i));
+
+
 class Solution {
     public int maxVowels(String s, int k) {
-        
-        // 0 basic variables
+         
         int max = 0;
         int count = 0;
 
-        // 1st window in max  and increase count , then only u can remove left side elements representation
         for (int i=0;i<k;i++) {if(isvow(s.charAt(i))) {count+=1;}}
         max=count; // it is must for the logic 
-        // 2nd  next windows , move one by one till last , so i<length of s
+        
         for (int i=k;i<s.length();i++) {
-        // 3rd count = count - isvow(s.charAt(i-k)) + isvow(s.charAt(i));
+        
            if (isvow(s.charAt(i-k))) count-=1;
            if (isvow(s.charAt(i))) count+=1;
 
