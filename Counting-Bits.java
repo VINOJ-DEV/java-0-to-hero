d899951e12419e6fa2f7832e0b81ba578aaec7d8
VINOJ S A <230801238@rajalakshmi.edu.in>
2025-07-27 22:48:39 +0530

    [338] [Time Beats: 94.16%] [Memory Beats: 83.97%] - LeetPush

--- Counting-Bits.java
+++ Counting-Bits.java
@@ -4,7 +4,7 @@ class Solution {
         int[] ans = new int[n+1];
         for(int i=0;i<=n;i++){
 
-            ans[i] = ans[i>>1]+(i&1);
+            ans[i] = ans[i/2]+ (i%2 == 1 ? 1 : 0);
         }
         
         return ans;
